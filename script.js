5dc6dac8e9bd2e6e107c599d6555498424b289af
Harsh Bhakre <140039147+Harshbhakre@users.noreply.github.com>
2025-07-13 11:17:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(arr){
+ let cost = 0
+ let n = arr.length
+ for(let i =0;i<n-1;i++){
+        arr = arr.sort((a,b)=>a-b)
+        let min1 = arr.shift()
+        let min2 = arr.shift()
+        arr.push(min1+min2)
+        cost += min1+min2
+        console.log(arr)
+    }
+    return cost
 }
 
 module.exports=mincost;
